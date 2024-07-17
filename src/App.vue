05c0c3eaf833a6de1b4d20393d3c978ca802f58e
yevhensh01.kodehode <yevhensh01.kodehode@gmail.com>
2024-07-17 11:56:20 +0000

    useTitle

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { useTitle } from '@vueuse/core';
 const title = useTitle();
+title.value = 'VueUse rocks!'
 console.log('page title', title.value);
 </script>
 
 <template>
-  <input type="text" />
+  <input type="text" v-model="title" />
 </template>
 
 <style scoped>
