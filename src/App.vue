6d61c087d013274d5012fd7edc98ef0884056898
yevhensh01.kodehode <yevhensh01.kodehode@gmail.com>
2024-07-17 12:54:55 +0000

    useColorMode for theme

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
 <script setup>
-import {ref} from 'vue';
-import { useClipboard } from '@vueuse/core';
-
-const textToCopy = ref('Hello');
-const { copy, copied, text } = useClipboard({source: textToCopy, copiedDuring: 3000,})
-
+import { useDark, useToggle, useColorMode } from '@vueuse/core'
+const isDark = useDark(); // true or false
+const colorMode = useColorMode({
+    modes: {
+        dim: 'dim',
+        cafe: 'cafe',
+    },
+    attribute: 'theme',
+}); // light or dark
+const toggleDark = useToggle(isDark);
 </script>
 
 <template>
-  <input type="text" v-model="textToCopy" />
-  <button @click="copy(textToCopy)">{{ copied ? 'Copied' : 'Copy'}}</button>
-  {{ text }}
+    {{ colorMode }}
+    <button @click="colorMode = 'dark'">Dark Mode</button>
+    <button @click="colorMode = 'light'">Light Mode</button>
+    <button @click="colorMode = 'cafe'">Cafe Mode</button>
+    <button @click="colorMode = 'dim'" >Dim Mode</button>
 </template>
 
-<style scoped>
-
+<style>
+[theme="dark"]{
+    background: black;
+    color: white;
+}
+[theme="light"].dark{
+    background: black;
+    color: white;
+}
+[theme='dim']{
+    background: rgb(0, 212, 71);
+    color: rgb(0, 0, 0);
+}
+[theme='cafe']{
+    background: burlywood;
+    color: rgb(65, 26, 26);
+}
 </style>
