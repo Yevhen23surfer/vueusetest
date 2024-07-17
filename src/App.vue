b7775f94e87aeee290a7e99271f46e13f0145d99
yevhensh01.kodehode <yevhensh01.kodehode@gmail.com>
2024-07-17 11:47:11 +0000

    vueusetest

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useTitle } from '@vueuse/core';
+const title = useTitle();
+console.log('page title', title.value);
+</script>
+
+<template>
+  <input type="text" />
+</template>
+
+<style scoped>
+
+</style>
