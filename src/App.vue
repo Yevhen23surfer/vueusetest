f99c52e835065b97de8645484144e0724a7634af
yevhensh01.kodehode <yevhensh01.kodehode@gmail.com>
2024-07-18 07:24:19 +0000

    useDisplayMedia - screen share, useUserMedia - camera etc , useMediaControls - video controls etc

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,15 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useInfiniteScroll } from '@vueuse/core';
-
-const el = ref<HTMLElement>(null);
-const data = ref([]);
-const total = ref(0);
-
-useInfiniteScroll(el, loadData, { distance: 10 });
-
-loadData();
-
-async function loadData() {
-  if (data.value.length !== 0 && data.value.length >= total.value) return;
-  const res = await fetch(
-    `https://dummyjson.com/products/?limit=10&skip=${data.value.length}`
-  );
-  const d = await res.json();
-  total.value = d.total;
-  data.value = data.value.concat(d.products);
-}
+import { ref, watchEffect } from 'vue';
+import { useDisplayMedia } from '@vueuse/core';
+const video = ref();
+const { stream, enabled } = useDisplayMedia();
+watchEffect(() => {
+  if (video.value) 
+    video.value.srcObject = stream.value;
+});
 </script>
 
 <template>
-  <div ref="el" class="el">
-    <div v-for="item in data" class="card">
-      <img :src="item.thumbnail" alt="" />
-      <h3>{{ item.title }}</h3>
-      <p>{{ item.description }}</p>
-    </div>
-  </div>
+  <button @click="enabled = !enabled">Start Screen Share</button>
+  <video ref="video" muted autoplay controls width="800"></video>
 </template>
-
-<style>
-html,
-body,
-#app {
-  height: 100%;
-}
-.el {
-  height: 100%;
-  overflow-y: scroll;
-}
-.card {
-  margin: 30px 10px;
-  box-shadow: 0 0 3px 4px rgba(0, 0, 0, 0.1);
-  border-radius: 10px;
-  padding: 20px;
-}
-img {
-  border-radius: 10px;
-  display: block;
-  width: 100%;
-}
-</style>
