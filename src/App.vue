f2c87e31bce6e0fe8b4b3ad9372ff5196ea40cdb
yevhensh01.kodehode <yevhensh01.kodehode@gmail.com>
2024-07-18 06:55:27 +0000

    onLongPress - Mouse & Touch with VueUse

--- src/App.vue
+++ src/App.vue
@@ -6,26 +6,38 @@
 //     unwatch();
 // });
 
-import { usePointer } from '@vueuse/core';
-import { onMounted, reactive, ref, watch } from 'vue';
-const pointer = reactive(usePointer());
-const canvas = ref  (null);
-watch(pointer, () => {
-    if(pointer.pressure === 0) return
-    const ctx = canvas.value.getContext('2d');
-    ctx.fillStyle = 'red';
-    ctx.fillRect(pointer.x - 20, pointer.y -20, 10, 10);
-});
-onMounted(()=> {
-    canvas.value.height = document.body.clientHeight;
-    canvas.value.width = document.body.clientWidth;
-})
+// import { usePointer } from '@vueuse/core';
+// import { onMounted, reactive, ref, watch } from 'vue';
+// const pointer = reactive(usePointer());
+// const canvas = ref  (null);
+// watch(pointer, () => {
+//     if(pointer.pressure === 0) return
+//     const ctx = canvas.value.getContext('2d');
+//     ctx.fillStyle = 'red';
+//     ctx.fillRect(pointer.x - 20, pointer.y -20, 10, 10);
+// });
+// onMounted(()=> {
+//     canvas.value.height = document.body.clientHeight;
+//     canvas.value.width = document.body.clientWidth;
+// })
 
+
+import { ref } from 'vue';
+import { onLongPress } from '@vueuse/core';
+const longBtn = ref(null);
+onLongPress(longBtn, () => {
+    alert('long press');
+},
+{
+    delay: 3000,
+}
+);
 </script>
 
 <template>
 <!-- {{ isLeft }} -->
-<canvas ref="canvas"></canvas>
+<!-- <canvas ref="canvas"></canvas> -->
+<button ref="longBtn">Click and Hold</button>
 </template>
 
 <style>
