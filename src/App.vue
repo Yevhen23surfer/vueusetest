266924576b5b17e9dfc6db5221f7951a4fdc09b3
yevhensh01.kodehode <yevhensh01.kodehode@gmail.com>
2024-07-18 06:49:56 +0000

    usePointer for example for painting

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,33 @@
 <script setup lang="ts">
-// import { useBattery } from '@vueuse/core';
-// const battery = useBattery();
+// import { usePageLeave, whenever } from '@vueuse/core';
+// const isLeft = usePageLeave();
+// const unwatch = whenever(isLeft, () => {
+//     alert('Are you sure you want to go?');
+//     unwatch();
+// });
 
-// import { useOnline } from '@vueuse/core';
-// const online = useOnline();
+import { usePointer } from '@vueuse/core';
+import { onMounted, reactive, ref, watch } from 'vue';
+const pointer = reactive(usePointer());
+const canvas = ref  (null);
+watch(pointer, () => {
+    if(pointer.pressure === 0) return
+    const ctx = canvas.value.getContext('2d');
+    ctx.fillStyle = 'red';
+    ctx.fillRect(pointer.x - 20, pointer.y -20, 10, 10);
+});
+onMounted(()=> {
+    canvas.value.height = document.body.clientHeight;
+    canvas.value.width = document.body.clientWidth;
+})
 
-import { useGeolocation } from '@vueuse/core';
-const { coords, locatedAt, error } = useGeolocation();
 </script>
 
 <template>
- <!-- {{ battery }} -->
- <!-- Online: {{ online }} -->
-
-    <pre lang="json">{{ 
-    JSON.stringify(
-        {
-            coords: {
-                accuracy: coords.accuracy,
-                lalitude: coords.lalitude,
-                longtitude: coords.longtitude,
-                altitude: coords.altitude,
-                altitudeAccuracy: coords.altitudeAccuracy,
-                heading: coords.heading,
-                speed: coords.speed,
-            },
-            locatedAt: locatedAt,
-            error: error ? error.message : error,
-        },
-        null,
-    ) }}</pre>
+<!-- {{ isLeft }} -->
+<canvas ref="canvas"></canvas>
 </template>
 
 <style>
-* {
-    font-size: 1.5rem;
-}
+
 </style>
