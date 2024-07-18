4fa76f391e618ad3037f4d6701046c818b2b9890
yevhensh01.kodehode <yevhensh01.kodehode@gmail.com>
2024-07-18 07:13:05 +0000

    useInfiniteScroll

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,55 @@
 <script setup lang="ts">
-// import { usePageLeave, whenever } from '@vueuse/core';
-// const isLeft = usePageLeave();
-// const unwatch = whenever(isLeft, () => {
-//     alert('Are you sure you want to go?');
-//     unwatch();
-// });
-
-// import { usePointer } from '@vueuse/core';
-// import { onMounted, reactive, ref, watch } from 'vue';
-// const pointer = reactive(usePointer());
-// const canvas = ref  (null);
-// watch(pointer, () => {
-//     if(pointer.pressure === 0) return
-//     const ctx = canvas.value.getContext('2d');
-//     ctx.fillStyle = 'red';
-//     ctx.fillRect(pointer.x - 20, pointer.y -20, 10, 10);
-// });
-// onMounted(()=> {
-//     canvas.value.height = document.body.clientHeight;
-//     canvas.value.width = document.body.clientWidth;
-// })
-
-
 import { ref } from 'vue';
-import { onLongPress } from '@vueuse/core';
-const longBtn = ref(null);
-onLongPress(longBtn, () => {
-    alert('long press');
-},
-{
-    delay: 3000,
+import { useInfiniteScroll } from '@vueuse/core';
+
+const el = ref<HTMLElement>(null);
+const data = ref([]);
+const total = ref(0);
+
+useInfiniteScroll(el, loadData, { distance: 10 });
+
+loadData();
+
+async function loadData() {
+  if (data.value.length !== 0 && data.value.length >= total.value) return;
+  const res = await fetch(
+    `https://dummyjson.com/products/?limit=10&skip=${data.value.length}`
+  );
+  const d = await res.json();
+  total.value = d.total;
+  data.value = data.value.concat(d.products);
 }
-);
 </script>
 
 <template>
-<!-- {{ isLeft }} -->
-<!-- <canvas ref="canvas"></canvas> -->
-<button ref="longBtn">Click and Hold</button>
+  <div ref="el" class="el">
+    <div v-for="item in data" class="card">
+      <img :src="item.thumbnail" alt="" />
+      <h3>{{ item.title }}</h3>
+      <p>{{ item.description }}</p>
+    </div>
+  </div>
 </template>
 
 <style>
-
+html,
+body,
+#app {
+  height: 100%;
+}
+.el {
+  height: 100%;
+  overflow-y: scroll;
+}
+.card {
+  margin: 30px 10px;
+  box-shadow: 0 0 3px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  padding: 20px;
+}
+img {
+  border-radius: 10px;
+  display: block;
+  width: 100%;
+}
 </style>
