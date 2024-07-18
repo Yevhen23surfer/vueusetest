5878775bb9e42860f22aa266cf04a7d7904995ea
yevhensh01.kodehode <yevhensh01.kodehode@gmail.com>
2024-07-18 06:06:29 +0000

    useMagicKeys  - The Keyboard and VueUse (Part 2)

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,16 @@
 <script setup>
-import {ref} from 'vue';
-import { onKeyStroke } from '@vueuse/core';
+import { ref, watch } from 'vue';
+import { useMagicKeys, whenever } from '@vueuse/core';
 
-const controls = {
-    ArrowDown: () => (position.value.y += 5),
-    ArrowUp: () => (position.value.y -= 5),
-    ArrowRight: () => (position.value.x += 5),
-    ArrowLeft: () => (position.value.x -= 5),
-};
+const { ctrl_c } = useMagicKeys();
+whenever(ctrl_c, (pressed) => console.log(pressed));
 
-onKeyStroke(Object.keys(controls), (e) =>{
-    e.preventDefault();
-    controls[e.key]();
-});
-
-const position = ref({ x: 0, y: 0 });
 </script>
 
 <template>
-    <div class="ball"></div>
+
 </template>
 
 <style>
-.ball {
-    width: 10px;
-    height: 10px;
-    border-radius: 50%;
-    background-color: red;
-    position: absolute;
-    top: v-bind(position.y + 'px');
-    left: v-bind(position.x + 'px');
-}
+
 </style>
