28dd38f3ca9ea95f0066b69e0bcd2a9a5aeb8706
yevhensh01.kodehode <yevhensh01.kodehode@gmail.com>
2024-07-18 06:28:00 +0000

    Device Sensors and VueUse battery, online, useGeolocation

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,39 @@
-<script setup>
-import { ref, watch } from 'vue';
-import { useMagicKeys, whenever } from '@vueuse/core';
+<script setup lang="ts">
+// import { useBattery } from '@vueuse/core';
+// const battery = useBattery();
 
-const { ctrl_c } = useMagicKeys();
-whenever(ctrl_c, (pressed) => console.log(pressed));
+// import { useOnline } from '@vueuse/core';
+// const online = useOnline();
 
+import { useGeolocation } from '@vueuse/core';
+const { coords, locatedAt, error } = useGeolocation();
 </script>
 
 <template>
+ <!-- {{ battery }} -->
+ <!-- Online: {{ online }} -->
 
+    <pre lang="json">{{ 
+    JSON.stringify(
+        {
+            coords: {
+                accuracy: coords.accuracy,
+                lalitude: coords.lalitude,
+                longtitude: coords.longtitude,
+                altitude: coords.altitude,
+                altitudeAccuracy: coords.altitudeAccuracy,
+                heading: coords.heading,
+                speed: coords.speed,
+            },
+            locatedAt: locatedAt,
+            error: error ? error.message : error,
+        },
+        null,
+    ) }}</pre>
 </template>
 
 <style>
-
+* {
+    font-size: 1.5rem;
+}
 </style>
