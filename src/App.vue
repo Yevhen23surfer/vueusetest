e649c64f47bce38ec230c032fc3540f69c905271
yevhensh01.kodehode <yevhensh01.kodehode@gmail.com>
2024-07-18 10:00:10 +0000

    useTransition composable from VueUse in order to transition between 2 different numbers

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,35 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue';
-import { useNow, useDateFormat } from '@vueuse/core';
-
-const now = useDateFormat(useNow(), "HH:mm:ss");
-
+import { ref } from 'vue';
+import { useTransition, TransitionPresets } from '@vueuse/core';
+const percent = ref(0);
+const disabled = ref(false);
+const color = ref('blue');
+const animatedPercent = useTransition(percent, {
+  transition: TransitionPresets.easeInCubic,
+  duration: 3000,
+  disabled,
+  onStarted() {},
+  onFinished() {
+    color.value = 'green';
+  },
+});
+const load = () => {
+  percent.value = 100;
+};
 </script>
 
 <template>
-{{ now }}
-
+  <div
+    class="loading"
+    :style="`width: ${animatedPercent}%; background: ${color};`"
+  ></div>
+  {{ Math.floor(animatedPercent) }}
+  <button @click="load">Load</button>
+  <button @click="disabled = true">Disable</button>
 </template>
+
+<style>
+.loading {
+  height: 10px;
+}
+</style>
