e48fe998d5e88cb4fe87e486165bc293db2c82e0
yevhensh01.kodehode <yevhensh01.kodehode@gmail.com>
2024-07-18 11:05:01 +0000

    useFetch and Reactify

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
+
 <script setup lang="ts">
-import { useLocalStorage, useSessionStorage } from '@vueuse/core';
-const name = useLocalStorage('name', 'William');
-const favoriteFood = useSessionStorage('favoriteFood', 'pizza');
+import { ref, computed } from 'vue';
+import { useFetch } from '@vueuse/core';
+
+const id = ref(1);
+const url = computed(() => {
+    return `https://jsonplaceholder.typicode.com/todos/${id.value}` 
+});
+const { isFetching, data, error } = useFetch(url, {
+    refetch: true
+});
 </script>
 
 <template>
-    <input type="text" v-model="name" />
-    <input type="text" v-model="favoriteFood" />
+<div v-if="error" style="color: red">Error {{ error }}</div>
+<div v-else-if="isFetching" style="color: blue">loading...</div>
+<pre v-else>{{ data }}</pre>
+<input type="number" v-model="id">
 </template>
+
+// good for pagination and serch etc
