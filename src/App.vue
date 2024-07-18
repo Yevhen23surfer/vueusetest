d435da243d40de9511f977e2f47f04ee059cbbb9
yevhensh01.kodehode <yevhensh01.kodehode@gmail.com>
2024-07-18 10:45:22 +0000

    useLocalStorage, useSessionStorage

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,10 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { useRefHistory, useDebouncedRefHistory } from '@vueuse/core';
-const newTodo = ref('');
-const todos = ref([]);
-const { history, undo, redo } = useRefHistory(todos, {
-  deep: true,
-  capacity: 15,
-});
+import { useLocalStorage, useSessionStorage } from '@vueuse/core';
+const name = useLocalStorage('name', 'William');
+const favoriteFood = useSessionStorage('favoriteFood', 'pizza');
 </script>
 
 <template>
-  <input type="text" placeholder="New Todo" v-model="newTodo" />
-  <button
-    @click="
-      todos.unshift(newTodo);
-      newTodo = '';
-    "
-  >
-    Create Todo
-  </button>
-  <button @click="undo">Undo</button>
-  <button @click="redo">Redo</button>
-  <ul>
-    <li v-for="todo in todos" :key="todo">{{ todo }}</li>
-  </ul>
-  <br />
-  {{ history }}
+    <input type="text" v-model="name" />
+    <input type="text" v-model="favoriteFood" />
 </template>
